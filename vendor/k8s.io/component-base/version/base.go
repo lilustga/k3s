5aea33dbc3ad02623fa88a34cf8c3cd5ff072526
Lior Lustgarten <lilustga@microsoft.com>
2022-05-17 16:54:49 -0700

    Added go mod vendor

---
+++ vendor/k8s.io/component-base/version/base.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	gitMajor = "1"
+	gitMinor = "23"
+	gitVersion   = "v1.23.6-k3s1"
+	gitCommit    = "a46fe77103d2dec7caba636211988899b454376f"
+	gitTreeState = "clean"
+	buildDate = "2022-04-20T19:32:28Z"
+)
